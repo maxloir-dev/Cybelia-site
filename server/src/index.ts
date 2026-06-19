bf2e94e0a368706b3d6f61903fa1329cd54fdf40
Maxime Loir <maxloir@hotmail.fr>
2026-06-19 22:16:35 +0200

    Bug regler pour la taille des photos

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,7 +32,8 @@ app.use(cors({
 	origin: process.env.CLIENT_URL,
 	methods: ["GET", "POST", "PUT", "DELETE"],
 }));
-app.use(express.json()); // Permet de lire le JSON dans les requêtes
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Rate limiting sur les routes sensibles
 const authLimiter = rateLimit({
